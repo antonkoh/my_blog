c1d4bbc46cf6da197ee24f79f2698c488b84f1c9
Anton Koh <anton.koh@gmail.com>
2015-12-06 21:03:46 +0000

    Lesson 21

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def link_with_icon(path,icon,options={})
+    title = options.delete(:title)
+    link_to path, options do
+      content_tag(:i," #{title}",class: "fa fa-#{icon}")
+    end
+  end
+
+
 end
